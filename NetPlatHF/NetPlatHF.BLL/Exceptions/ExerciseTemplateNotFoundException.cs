5e07caf362e0739ae8dea8a9b04116736ed73c76
Martin Balázs <40437323+Martinovics@users.noreply.github.com>
2023-05-11 19:06:23 +0200

    Create ExerciseTemplateNotFoundException.cs

---
+++ NetPlatHF/NetPlatHF.BLL/Exceptions/ExerciseTemplateNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿namespace NetPlatHF.BLL.Exceptions;
+
+
+
+
+public class ExerciseTemplateNotFoundException : Exception
+{
+    public ExerciseTemplateNotFoundException()
+    {
+    }
+
+    public ExerciseTemplateNotFoundException(string message) : base(message)
+    {
+    }
+
+    public ExerciseTemplateNotFoundException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
